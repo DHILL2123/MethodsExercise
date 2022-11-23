2fbd4e272388b4e6a325d0d668dc2441f15e2520
DeAdrien Hill <deadrienhill@yahoo.com>
2022-11-23 11:08:10 -0600

    created user profile

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace MethodsExercise
+﻿using Internal;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine(); //input
+
+            Console.WriteLine($"{name} What is your favorite color?");
+
+            string favoriteColor = Console.ReadLine(); //input
+
+            Console.WriteLine($"{favoriteColor} Is also my favorite color. What is your favorite animal?");
+
+            string favoriteAnimal = Console.ReadLine(); //input
+
+            Console.WriteLine($"I'm a cat person myself. What is your favorite band?");
+
+            string favoriteBand = Console.ReadLine(); //input
+
+            Console.WriteLine($"{favoriteBand}, That's cool I'm also a fan. Where were you born?");
+
+            string bornIn = Console.ReadLine(); //input
+
+            Console.WriteLine($"{bornIn}? My sister lives there currently. Where do you live now?");
+
+            string liveIn = Console.ReadLine(); //input
+
+            Console.WriteLine($"{name}, thanks for your input. Here is your profile.");
+
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine($"Name: {name}");
+
+            Console.WriteLine($"Favorite Color: {favoriteColor}");
+
+            Console.WriteLine($"Favorite Animal: {favoriteAnimal}");
+
+            Console.WriteLine($"Favorite Band: {favoriteBand}");
+
+            Console.WriteLine($"Born: {bornIn}");
+
+            Console.WriteLine($"Reside: {liveIn}");
+
+
         }
     }
 }
